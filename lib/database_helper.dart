322934e4e7c914300f078bc8294a459a6c402838
Nabeel Azhar <bale@Nabeels-Laptop.local>
2025-03-21 17:55:35 +0800

    feat: add SQLite database for backend storing

---
+++ lib/database_helper.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'todo.db');
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE todos(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)',
+        );
+      },
+    );
+  }
+
+  Future<void> addTask(String title) async {
+    final db = await database;
+    await db.insert('todos', {'title': title});
+  }
+
+  Future<List<Map<String, dynamic>>> getTasks() async {
+    final db = await database;
+    return db.query('todos');
+  }
+}
